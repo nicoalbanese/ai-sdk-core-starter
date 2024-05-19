d997d8e3442619e05e45b3d96ed9e2663e8755ab
Nico Albanese <nico.albanese@vercel.com>
2024-05-19 10:42:05 +0100

    init

---
+++ src/generate-text.ts
@@ -0,0 +1,6 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+async function main() {}
+main().catch(console.error);
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+async function main() {
+  console.log(process.env.OPENAI_API_KEY);
+}
+main().catch(console.error);
